ddebbdff8ce7ffbed40e157e759eb235917656ae
Vinicius Rossi <vini.rossisilva@gmail.com>
2023-01-06 17:25:01 -0300

    feat: create components of view matches and standings

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,12 +6,18 @@ import { AppComponent } from './app.component';
 import { HeaderComponent } from './core/components/header/header.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { FooterComponent } from './core/components/footer/footer.component';
+import { MainComponent } from './core/components/main/main.component';
+import { MatchesComponent } from './features/matches/matches.component';
+import { StandingsComponent } from './features/standings/standings.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
-    FooterComponent
+    FooterComponent,
+    MainComponent,
+    MatchesComponent,
+    StandingsComponent
   ],
   imports: [
     BrowserModule,
