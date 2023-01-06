2a7f10a18e03fa9eddaa09b7f9ce69dba5f9221b
Vinicius Rossi <vini.rossisilva@gmail.com>
2023-01-06 17:30:19 -0300

    chore: insert path for pages

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,18 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { StandingsComponent } from './features/standings/standings.component';
+import { MatchesComponent } from './features/matches/matches.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: "matches",
+    component: MatchesComponent
+  },
+  {
+    path: "standings",
+    component: StandingsComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
