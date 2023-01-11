81e15a8180cc804c0b766f1e11d34ce1ac3b6f8d
Vinicius Rossi <vini.rossisilva@gmail.com>
2023-01-11 08:07:05 -0300

    chore: import config api fake with db.json

--- src/app/features/standings/standings.component.ts
+++ src/app/features/standings/standings.component.ts
@@ -1,10 +1,23 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Standings } from 'src/app/shared/standings.model';
+import { StandingsService } from 'src/app/shared/standings.service';
 
 @Component({
   selector: 'app-standings',
   templateUrl: './standings.component.html',
   styleUrls: ['./standings.component.scss']
 })
-export class StandingsComponent {
+export class StandingsComponent implements OnInit {
+
+  clubs: Standings[];
+  displayedColumns: string[] = ['id', 'urlBadge', 'club'];
+
+  constructor(private standingsService: StandingsService) { }
+
+  ngOnInit(): void {
+    this.standingsService.read().subscribe(clubs => {
+      this.clubs = clubs
+    })
+  }
 
 }
