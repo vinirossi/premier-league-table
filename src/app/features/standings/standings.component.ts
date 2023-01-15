15d6f01c8cb7762eb98c8b4d1d2dafa4c12ebf94
Vinicius Rossi <vini.rossisilva@gmail.com>
2023-01-14 23:19:59 -0300

    chore: developed componet and method for show view league table

--- src/app/features/standings/standings.component.ts
+++ src/app/features/standings/standings.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
-import { Standings } from 'src/app/shared/standings.model';
+// import { Observable } from 'rxjs';
+// import { LeagueTable } from 'src/app/shared/standings.model';
 import { StandingsService } from 'src/app/shared/standings.service';
 
 @Component({
@@ -8,16 +9,27 @@ import { StandingsService } from 'src/app/shared/standings.service';
   styleUrls: ['./standings.component.scss']
 })
 export class StandingsComponent implements OnInit {
-
-  clubs: Standings[];
-  displayedColumns: string[] = ['id', 'urlBadge', 'club'];
+  // standings!: any;
+  // clubs: LeagueTable[];
+  clubs: any = [];
+  displayedColumns: string[] = ['position', 'club', 'points', 'played', 'won', 'drawn', 'lost', 'goalsFor', 'goalsAgainst', 'goalsDifference'];
 
   constructor(private standingsService: StandingsService) { }
 
   ngOnInit(): void {
-    this.standingsService.read().subscribe(clubs => {
-      this.clubs = clubs
+    this.standingsService.read().subscribe((clubs: any) => {
+      this.clubs = clubs.response
+      console.log(this.clubs[0].league.standings, 'funfa pow')
     })
+
+    // this.standings = this.standingsService.read();
+    // console.log(this.standings, 'mostrar table')
+
+    // this.standingsService.read().subscribe((clubs: any) => {
+    //   this.clubs = clubs
+    //   console.log(this.clubs, 'funfa pow')
+    // })
+    // this.standingsService.read();
   }
 
 }
