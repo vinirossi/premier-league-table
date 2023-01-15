040474d8b87eed0568a3f5ea0f446b590169ccfd
Vinicius Rossi <vini.rossisilva@gmail.com>
2023-01-14 23:20:35 -0300

    feat: developed service with information api rest

--- src/app/shared/standings.model.ts
+++ src/app/shared/standings.model.ts
@@ -1,5 +1,32 @@
-export interface Standings {
-  urlBadge: string
-  id: number
-  club: string
+export interface LeagueTable {
+  // league: League
+  // team: Team
+  // venue: Venue
+  // position: number
+  // crestURI: string
 }
+
+// export interface League {
+//   standings: string
+// }
+
+// export interface Table {
+//   position: number
+//   team: Team
+// }
+
+// export interface Team {
+//   // id: number
+//   name: string
+// }
+
+// export interface Team {
+//   id: number
+//   name: string
+//   logo: string
+// }
+
+// export interface Venue {
+//   id: number
+//   name: string
+// }
--- src/app/shared/standings.service.ts
+++ src/app/shared/standings.service.ts
@@ -1,4 +1,4 @@
-import { Standings } from './standings.model';
+// import { LeagueTable } from './standings.model';
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from "rxjs";
@@ -8,12 +8,35 @@ import { Observable } from "rxjs";
 })
 export class StandingsService {
 
-  baseUrl = "http://localhost:3000/standings"
+  // baseUrl = "https://v3.football.api-sports.io/standings?league=39&season=2022"
+    baseUrl = "https://api.football-data.org/v2/competitions/PL/standings/"
+  // baseUrl = "http://localhost:3000/standings"
+  // baseUrl = "https://v3.football.api-sports.io/teams?league=39&season=2022"
+  // baseUrl = "https://api.football-data-api.com/league-tables?key=test85g57&season_id=1625"
+
+
 
   constructor(
     private http: HttpClient) { }
 
-  read(): Observable<Standings[]> {
-    return this.http.get<Standings[]>(this.baseUrl)
+  read(): Observable<any> {
+     return this.http.get<any>(this.baseUrl, {
+      headers: {
+        'X-Auth-Token': '198aae5542e24ceaa7ad4bb54ad561a6'
+      }
+     });
   }
+
+  // read(): Observable<Standings[]> {
+  //   return this.http.get<Standings[]>(this.baseUrl)
+  // }
+
+  // read(): Observable<any> {
+  //   return this.http.get<any>(this.baseUrl, {
+  //     headers: {
+  //       'x-rapidapi-key': '8f1e80779c668a93f60a9a5c44754407',
+  //       'x-rapidapi-host': 'api-football-v1.p.rapidapi.com'
+  //     }
+  //   });
+  // }
 }
